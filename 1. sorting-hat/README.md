0204f8688d73290b5c9d020d908f76440a05fdeb
sdk <s.danyal.k@gmail.com>
2023-09-22 15:37:40 -0700

    added first activity

---
+++ 1. sorting-hat/README.md
@@ -0,0 +1,15 @@
+# Sorting Hat Ceremony Activity
+    Headmaster of Hogwarts School of Witchcraft and Wizardry reached out to bring their Sorting Hat ceremony
+    to the digital era. He would like the ceremony to be computerized instead of manual process.
+
+    Sorting Hat is a magical hat that decides which house do the first year students go to. These houses are
+    Gryffindor, Slytherin, Ravenclaw, and Hufflepuff.
+
+    Sorting Hat uses student's personality to determine which house the student belongs to.
+
+    You are going to write a program that does the work of Sorting Hat.
+
+### TODO items
+1. Create a new Java project called `sorting-hat`.
+2. Copy and replace the folder `src` from the folder `1. sorting-hat` into your newly created project.
+3. Complete the TODOs mentioned in the `Main.java`.
---
+++ 1. sorting-hat/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO 1. Create integer variable called studentPersonality and assign it default value of 1.
+        // TODO 2. Create 3 integer variables which will represent personality of daring, ambitious, and clever.
+        // TODO 3. Assign numbers to each personality. For example: int daring = 1;
+
+        // NOTE - We are using integer instead of String. There is a reason for it, which we will discuss later.
+
+        // TODO 4. If the student's personality is daring, print "You belong in Gryffindor!!!"
+        // TODO 5. Else if the student's personality is ambitious, print "You belong in Slytherin!!!"
+        // TODO 6. Else if the student's personality is clever, print "You belong in Ravenclaw!!!"
+        // TODO 7. Otherwise print "You belong in Ravenclaw"=!!!"
+        // TODO 8. Test the functionality by changing the value of the variable studentPersonality and see output in the console.
+    }
+}
